8e6a4bedffbb381000a7f61509e76b609de90166
Adrian Kristoffer Borgund <Adrian.Kristoffer.Borgund@dnb.no>
2024-02-15 09:43:09 +0100

    Add stupid fullscreen hook

--- app/components/windowcontrols.tsx
+++ app/components/windowcontrols.tsx
@@ -1,4 +1,5 @@
 import styles from "./windowcontrols.module.css";
+import useFullscreen from "~/hooks/useFullscreen";
 
 type windowcontrolsProps = {
   on_close?: () => void;
@@ -6,17 +7,31 @@ type windowcontrolsProps = {
   on_maximize?: () => void;
 };
 
+// export function toggleFullScreen() {
+//   if (!document.fullscreenElement) {
+//     document.documentElement.requestFullscreen();
+//   } else if (document.exitFullscreen) {
+//     document.exitFullscreen();
+//   }
+// }
+
 export const Windowcontrols = ({
   on_close,
   on_minimize,
   on_maximize,
 }: windowcontrolsProps) => {
+  const [isFullscreen, toggleFullScreen] = useFullscreen();
   return (
     <>
       <div className={styles.windowControls}>
         <button className={styles.close} onClick={on_close}></button>
         <button className={styles.minimize} onClick={on_minimize}></button>
-        <button className={styles.maximize} onClick={on_maximize}></button>
+        <button
+          className={
+            styles.maximize + " " + (isFullscreen ? styles.fullscreen : "")
+          }
+          onClick={on_maximize ?? (() => toggleFullScreen())}
+        ></button>
       </div>
     </>
   );
